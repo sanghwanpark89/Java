42bcf96dd2c165147a21fbc1d1fe3bf7534dd286
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2021-08-12 16:29:40 -0400

    Update BinarySearch.java
    What is the Binary Search?
    
    Binary Search is a search a sorted array by repeatedly dividing the search interval in half. Begin with an interval covering the whole array.
    If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. 
    Otherwise, narrow it to the upper half.
    It will check until the value is found or the interval is empty.
    
    This code will return -1 if there is no target number.

--- BinarySearch.java
+++ BinarySearch.java
@@ -7,23 +7,27 @@ public class BinarySearch {
 
 		Scanner myScan = new Scanner(System.in);
 		System.out.println("Enter your array size");
+		//ASK Array Size to User
 		int arraySize = myScan.nextInt();
+		//User will enter array size
 		int[] myArray = new int[arraySize];
-
+		//Create array
 		System.out.println("Enter your array element");
-		
 		for(int i = 0; i < arraySize; i++){
 			myArray[i] = myScan.nextInt();
 		}
-		
+		//User will enter array element
+
 		System.out.println("Enter your target number");
 		
 		int target = myScan.nextInt();
+		//User needs to enter target number to have index number
 		
 		System.out.println(Arrays.toString(myArray));
-		
+		//Eclipse will print array
 		
 		System.out.println(binarySearch(myArray, target));
+		//This line will send info of array and target number
 		
 	}
 	
@@ -33,36 +37,50 @@ public class BinarySearch {
 			return -1;
 		}
 		
-		int s = 0;
-		int e = inputArray.length - 1;
-
-//		while(s <= e){
-//			int m =((s+e)/2);
-//
-//			if(inputArray[m] == target){
-//				return m;
-//			}
-//			else if(target < inputArray[m]){
-//				e = m-1;
-//			}
-//			else{
-//				s = m+1;
-//			}
-//		}
+		//if array length is equal to 0, program will be terminate automatically.
 		
-		for(int i = 0; i < inputArray.length; i++){
+		int s = 0;
+		//Starting point is 0 of Array
+		int e = inputArray.length - 1;
+		//Ending point is array length - 1
+
+		while(s <= e){
+			//if starting point is less than equal to ending point, program will be executed till starting point is equal to ending point
+			
 			int m =((s+e)/2);
-			if(inputArray[i] == target){
-				return i;
+			// mid point is starting point - eding point
+
+			if(inputArray[m] == target){
+				return m;
+				//if target number is equal to input of Array, it will return index number of array
 			}
 			else if(target < inputArray[m]){
-				e = m - 1;
+				e = m-1;
+				//if target is less than mid of array, ending point will be changed as m-1
 			}
 			else{
-				s = m + 1;
+				s = m+1;
+				//if target is greater than mid of array, starting point will be changed as m+1 since array index start with 0.
 			}
 		}
+		
+//		for(int i = 0; i < inputArray.length; i++){
+//			int m =((s+e)/2);
+//			if(inputArray[i] == target){
+//				return i;
+//			}
+//			else if(target < inputArray[m]){
+//				e = m - 1;
+//			}
+//			else{
+//				s = m + 1;
+//			}
+//		}
+		//we can use for loop. please refer it if you need.
+		
 		return -1;
+		
+		//this program will return -1, if target number is not in the array.
 	}
 
 }
