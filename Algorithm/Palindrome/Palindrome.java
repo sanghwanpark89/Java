0ce80bceb56b61c7180b5a7782b3beab25465e4b
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2021-08-20 15:45:31 -0400

    Add files via upload

--- Algorithm/Palindrome/Palindrome.java
+++ Algorithm/Palindrome/Palindrome.java
@@ -1,1 +1,47 @@
 
+import java.util.*;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		Scanner myScan = new Scanner(System.in);
+		
+		System.out.println("Enter your Number");
+		
+		int inputNumber = myScan.nextInt();
+		
+		System.out.println(isPalindrome(inputNumber));		
+	}
+	
+	public static boolean isPalindrome(int num){
+		
+		int myNum = num;
+		
+		if(num == 0){
+			return true;
+		}
+		
+		if( num < 0 || num %10 == 0){
+			return false;
+		}
+		
+		int reverseNumber = 0;
+		while(myNum > 0){
+			
+			reverseNumber = reverseNumber*10 + (myNum%10);
+			
+			myNum = myNum / 10;
+		}
+		
+		if(num == reverseNumber){
+			System.out.println(reverseNumber);
+			return true;
+		}
+		else{
+			System.out.println(reverseNumber);
+			return false;
+		}
+	}
+
+}
