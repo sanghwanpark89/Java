a897493150a41edf3af8c42cc8769b875dca632c
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2022-01-05 18:13:31 -0500

    Create TwoSum.java

---
+++ leetCode/twoSum/TwoSum.java
@@ -0,0 +1,21 @@
+package TwoSum;
+
+import java.util.*;
+
+public class TwoSum {
+	
+	public int[] twoSum(int[] nums, int target) {
+		
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i+1; j < nums.length; j++) {
+				if((nums[i] + nums[j])==target)
+					return new int[] {i,j};
+			}
+		}
+		//We need two for loop to check the sum of the each element will meet target number
+		//if the sum of the each element is meet target number, it will return the array with 
+
+		return null;
+		//if there is no result, this method will return null value.
+	}
+}
