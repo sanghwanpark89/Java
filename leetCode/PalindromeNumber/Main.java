488f006f79deaf1c3167af1e7287ef4c0b7bc9e4
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2022-01-08 02:59:19 -0500

    Update Main.java

--- leetCode/PalindromeNumber/Main.java
+++ leetCode/PalindromeNumber/Main.java
@@ -10,18 +10,24 @@ public class Main {
 		//Scanner instance
 		
 		System.out.println("Enter your String or Number");
+		//Ask to user to provide the input
 		
 		String myString = myScan.nextLine();
+		//myString variable will save the value from the user
 		
 		Solution mySolution = new Solution();
+		//call the instance
 		
 		System.out.println(mySolution.isPalindrome(myString));
+		//print the result
 		
 		if(mySolution.isPalindrome(myString)) {
 			System.out.println("Given input is palindrome");
+			//if input is palindrome, it will print above
 		}
 		else {
 			System.out.println("Given input is not palindrome");
+			//if input is not palindrome, it will print above
 		}
 		
 	}
