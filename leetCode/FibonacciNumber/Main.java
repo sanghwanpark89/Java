d468399772d5877a9ccad6c39627db6909eac55e
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2022-01-21 10:19:10 -0500

    Add files via upload

--- leetCode/FibonacciNumber/Main.java
+++ leetCode/FibonacciNumber/Main.java
@@ -7,13 +7,19 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner myScan = new Scanner(System.in);
+		//Create the scanner instance
 		
 		System.out.println("Enter your number");
+		//Ask to user to provide the number
 		
 		int myNum = myScan.nextInt();
+		//myNum will be have number which is from user
 		
 		Solution mySolution = new Solution();
+		//Create the solution instance
 		
 		System.out.println(mySolution.fib(myNum));
+		//print fibanocci number
 	}
+
 }
--- leetCode/FibonacciNumber/Solution.java
+++ leetCode/FibonacciNumber/Solution.java
@@ -5,11 +5,14 @@ public class Solution {
 	public int fib(int n) {
 		
 		if(n <= 1) {
+		//if n is less than equal to 1
 			
 			return n;
+			//return number n
 		}
 		
 		return fib(n-1) + fib(n-2);
+		//return n-1 + n-2
 		
 	}
 
