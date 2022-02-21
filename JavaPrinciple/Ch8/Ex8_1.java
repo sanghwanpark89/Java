25ca2a3a336f98aae048975d0ca59551dfa36b5e
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2022-02-20 19:16:27 -0500

    Create Ex8_1.java

---
+++ JavaPrinciple/Ch8/Ex8_1.java
@@ -0,0 +1,25 @@
+package Ch8;
+
+/*
+ * if there is error, this code will go through the catch statement
+ * if not, this code will skip the catch statement
+ */
+
+public class Ex8_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		
+		try {
+			System.out.println(2);
+			System.out.println(2);
+		}
+		catch(Exception e){
+			System.out.println(4);
+
+		}
+		//end of the try-catch statement
+		System.out.println(5);
+	}
+}
