a1e8d63347fc2bd7dbb4f6271613472eff258ef7
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-01-01 22:58:43 -0500

    Create Ex9_1.java

---
+++ JavaPrinciple/Ch9/Ex9_1.java
@@ -0,0 +1,26 @@
+package ch9;
+
+public class Ex9_1 {
+
+	public static void main(String[] args) {
+		
+		Value v1 = new Value(10);
+		Value v2 = new Value(10);
+		
+		if(v1.equals(v2)) {
+			System.out.println("v1 is equals to v2");
+		}
+		else {
+			System.out.println("v1 is different with v2");
+		}
+	}
+}
+
+class Value{
+	
+	int value;
+	
+	Value(int value){
+		this.value = value;
+	}
+}
