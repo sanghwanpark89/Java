6046b9f221b3a2cccdf82edaba30658609b4f538
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-03 18:37:35 -0400

    Create Ex6_8.java

---
+++ JavaPrinciple/Ch6/Ex6_8.java
@@ -0,0 +1,37 @@
+package ch06_Ex;
+
+public class Ex6_8 {
+
+	public static void main(String[] args) {
+		
+		Data3 d = new Data3();
+		//Create the Data3 instance
+		//variable d point to Data3 instance
+		
+		d.x = 10;
+		//variable d.x will have value 10
+		
+		Data3 d2 = copy(d);
+		//variable d2 will be created first then call copy method.
+		System.out.println("d.x = " + d.x);
+		//print 10
+		System.out.println("d2.x = " + d2.x);
+		//print 10
+
+	}
+	
+	static Data3 copy(Data3 d) {
+		//copy method will return address
+		Data3 tmp = new Data3();
+		//create variable tmp which is data3 data type
+		tmp.x = d.x;
+		//copy d.x value to tmp.x
+		return tmp;
+		//return address
+	}
+
+}
+
+class Data3{
+	int x;
+}
