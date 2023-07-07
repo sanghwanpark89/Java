5f0b1e49b4d43d22bac5b84aace021321d860445
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-06 22:04:19 -0400

    Create Ex6_10.java

---
+++ JavaPrinciple/Ch6/Ex6_10.java
@@ -0,0 +1,37 @@
+package ch06_Ex;
+
+public class Ex6_10 {
+
+	public static void main(String[] args) {
+		
+		MyMath3 mm = new MyMath3();
+		System.out.println("mm.add(3,3) result:" + mm.add(3,3));
+		System.out.println("mm.add(3,3) result:" + mm.add(3,3L));
+		System.out.println("mm.add(3,3) result:" + mm.add(3L,3));
+		System.out.println("mm.add(3,3) result:" + mm.add(3L,3L));
+
+
+	}
+
+}
+
+class MyMath3{
+	int add(int a, int b) {
+		System.out.print("int add(int a, int b) - ");
+		return a+b;
+	}
+	
+	long add(int a, long b) {
+		System.out.print("long add(int a, long b) - ");
+		return a+b;
+	}
+	
+	long add(long a, int b) {
+		System.out.print("long add(long a, int b) - ");
+		return a+b;
+	}
+	long add(long a, long b) {
+		System.out.print("long add(long a, long b) - ");
+		return a+b;
+	}
+}
