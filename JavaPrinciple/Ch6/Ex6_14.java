5d71ae3348831913a85130fa13c1540f0861202e
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-08 23:00:41 -0400

    Create Ex6_14.java

---
+++ JavaPrinciple/Ch6/Ex6_14.java
@@ -0,0 +1,32 @@
+package ch06_Ex;
+
+public class Ex6_14 {
+	
+	static {
+		System.out.println("static { }");
+	}
+	
+	{
+		System.out.println("{ }");
+	}
+	
+	public Ex6_14(){
+		System.out.println("Constructor");
+	}
+	
+//	Ex6_14(){
+//		System.out.println("Constructor");
+//	}
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ex6_14 bt = new Ex6_14(); ");
+		Ex6_14 bt = new Ex6_14();
+		
+		System.out.println("Ex6_14 bt2 = new Ex6_14(); ");
+		Ex6_14 bt2 = new Ex6_14();
+
+	}
+
+}
