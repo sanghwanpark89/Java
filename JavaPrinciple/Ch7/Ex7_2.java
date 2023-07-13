c32cf883f8a41048cf1f4f0478447dbf6cec2599
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-12 22:40:28 -0400

    Update Ex7_2.java

--- JavaPrinciple/Ch7/Ex7_2.java
+++ JavaPrinciple/Ch7/Ex7_2.java
@@ -1,28 +1,26 @@
-package ch7;
-
-class Point{	// Point class
-	int x;
-	int y;
-}
-
-class Circle extends Point{	//	This class will inherit the Point class
-	int r;					//	This class will have 3 member(2 members are from Point class and 1 member is from Point class)
-}
-
+package ch07_Ex;
 
 public class Ex7_2 {
 
 	public static void main(String[] args) {
 		
-		Circle c = new Circle();	//	Create the circle instance
-		
-		c.x = 3;	// x is 3
-		c.y = 4;	// y is 4
-		c.r = 5;	// r is 5
-		
-		System.out.println("c.x = " + c.x);
-		System.out.println("c.y = " + c.y);
-		System.out.println("c.r = " + c.r);
+		Child c = new Child();
+		c.method();
+
 	}
 
+}
+
+class Parent{
+	int x = 10;
+}
+
+class Child extends Parent{
+	int x = 20;
+	
+	void method() {
+		System.out.println("x = " + x);
+		System.out.println("this.x = " + this.x);
+		System.out.println("super.x= " + super.x);
+	}
 }
