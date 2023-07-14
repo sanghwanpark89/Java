6674b43e28ee080d9786b02a77b96e23ee99fa08
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-13 23:05:32 -0400

    Update Ex7_4.java

--- JavaPrinciple/Ch7/Ex7_4.java
+++ JavaPrinciple/Ch7/Ex7_4.java
@@ -1,25 +1,31 @@
-package ch7;
-
-class Point3{
-	int x;	//	variable
-	int y;	//	variable
-	
-	Point3(int x, int y){	//	Point3 Constructor to initialize variable
-		this.x = x;
-		this.y = y;
-	}
-	
-	public String toString() {	//	toString method will return string 
-		return "x: " + x + ", y: " + y;
-	}
-}
+package ch07_Ex;
 
 public class Ex7_4 {
 
 	public static void main(String[] args) {
 		
-		Point3 p = new Point3(3,5);
-		System.out.println(p);
+		Point3D p3 = new Point3D(1,2,3);
+		System.out.println("p3.x = " +p3.x + ", p3.y = " + p3.y + ", p3.z = " + p3.z);
+
 	}
 
+}
+
+class Point4{
+	int x;
+	int y;
+	
+	Point4(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class Point3D extends Point4{
+	int z;
+	
+	Point3D(int x, int y, int z){
+		super(x, y);
+		this.z = z;
+	}
 }
