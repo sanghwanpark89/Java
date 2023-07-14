d9def60d8dd65cf91cfece3567b572490eed65f2
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-13 22:57:28 -0400

    Update Ex7_3.java

--- JavaPrinciple/Ch7/Ex7_3.java
+++ JavaPrinciple/Ch7/Ex7_3.java
@@ -1,31 +1,23 @@
-package ch7;
-
-class Point2{
-	int x;
-	int y;
-}
-
-class Circle2{	//	Circle2 class has Point2 class
-				//	Composite
-	
-	Point2 p = new Point2();
-	int r;
-	
-}
+package ch07_Ex;
 
 public class Ex7_3 {
 
 	public static void main(String[] args) {
 		
-		Circle2 c = new Circle2();	//	Create the Circle2 instance
-		
-		c.p.x = 3;	//	if we need to access to variable x in Point2, we need to do c.p.x;
-		c.p.y = 4;	//	if we need to access to variable x in Point2, we need to do c.p.y;
-		c.r = 5;	//	r variable is from Circle2, so we only need c.r;
-		
-		System.out.println("c.p.x = " + c.p.x);
-		System.out.println("c.p.y = " + c.p.y);
-		System.out.println("c.r = " + c.r);
+		Child2 c2 = new Child2();
+		c2.method();
 	}
 
+}
+
+class Parent2{
+	int x = 10;
+}
+
+class Child2 extends Parent2{
+	void method() {
+		System.out.println("x = " + x);
+		System.out.println("this.x = " + this.x);
+		System.out.println("super.x = " + super.x);
+	}
 }
