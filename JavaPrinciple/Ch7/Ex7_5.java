0a5722283eb8bcc2db5adb6f57c615ce5f9663c2
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-14 22:30:11 -0400

    Update Ex7_5.java

--- JavaPrinciple/Ch7/Ex7_5.java
+++ JavaPrinciple/Ch7/Ex7_5.java
@@ -1,31 +1,9 @@
-package ch7;
-
-class Parent{
-	int x = 10;	// super.x
-}
-
-class Child extends Parent{
-	
-	//int x = 20;	//this.x
-	
-	/*
-	 * If there is no local variable in child class, the variable in parent class will be this.x
-	 */
-	
-	void method() {
-		System.out.println("x = " + x);
-		System.out.println("this.x = " + this.x);
-		System.out.println("super.x = " + super.x);
-	}
-}
-
+package ch07_Ex;	//Ex7_5 class will be in this package
 
 public class Ex7_5 {
 
 	public static void main(String[] args) {
-		
-		Child c = new Child();
-		c.method();
+		System.out.println("Hello World");
 
 	}
 
