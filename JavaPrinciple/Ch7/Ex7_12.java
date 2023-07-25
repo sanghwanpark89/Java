693a4b3d044ebd8ff7657b066c25a6b21ee2cd6d
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-24 22:28:06 -0400

    Update Ex7_12.java

--- JavaPrinciple/Ch7/Ex7_12.java
+++ JavaPrinciple/Ch7/Ex7_12.java
@@ -1,79 +1,29 @@
-package ch7;
-
-/*
- * Abstract class is created for common members from other class.
- */
-
-abstract class Unit{
-	int x;
-	int y;
-	abstract void move(int x, int y);//Moving method 
-	void stop() {
-	}
-}
-
-class Marine extends Unit{
-	
-	void move(int x, int y) {
-		super.x = x;
-		super.y = y;
-		System.out.println("Marine move to [x = " + x + ", y= " + y + "]");
-	}
-	void stop() {
-		System.out.println("Marine stop at [x = " + x + ", y= " + y + "]");
-	}
-}
-
-class Tank extends Unit{
-	void move(int x, int y) {
-		super.x = x;
-		super.y = y;
-		System.out.println("Tank move to [x = " + x + ", y= " + y + "]");
-	}
-	void changeMode() {
-		System.out.println("Tank change mode at [ x= " + x + ", y= " + y + "]");
-	}
-	void stop() {
-		System.out.println("Tank stop at [x = " + x + ", y= " + y + "]");
-	}
-}
-
-class Dropship extends Unit{
-	void move(int x, int y) {
-		super.x = x;
-		super.y = y;
-		System.out.println("Dropship move to [x = " + x + ", y= " + y + "]");
-	}
-	void load() {
-		System.out.println("Dropship load at [x = " + x + ", y= " + y + "]");
-	}
-	
-	void unload() {
-		System.out.println("Dropship unload at [x = " + x + ", y= " + y + "]");
-	}
-	void stop() {
-		System.out.println("Dropship stop at [x = " + x + ", y= " + y + "]");
-	}
-}
+package ch07_Ex;
 
 public class Ex7_12 {
+	
+	class InstanceInner{
+		int iv =100;
+		final static int CONST = 100;
+	}
+	
+	static class StaticInner{
+		int iv = 200;
+		static int cv = 200;
+	}
+	
+	void myMethod() {
+		class LocalInner{
+			int iv = 300;
+			final static int CONST = 300;
+		}
+	}
 
 	public static void main(String[] args) {
 		
-		Unit[] group = new Unit[3];
-		
-		group[0] = new Marine(); 
-		group[1] = new Tank();
-		group[2] = new Dropship();
-		
-		group[0].move(100,200);
-		group[1].move(100,200);
-		group[2].move(100,200);
-		
-		group[0].stop();
-		group[1].stop();
-		group[2].stop();
-		
+		System.out.println(InstanceInner.CONST);
+		System.out.println(StaticInner.cv);
+
 	}
 
 }
