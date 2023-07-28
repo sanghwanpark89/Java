bd9f40d72ab7b9603cb8fc6f3cd69d46c357f007
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-28 06:37:18 -0400

    Update Ex9_2.java

--- JavaPrinciple/Ch9/Ex9_2.java
+++ JavaPrinciple/Ch9/Ex9_2.java
@@ -1,41 +1,45 @@
-package ch9;
+package ch09_Ex;
+
+class Person extends Object{
+	
+	long id;
+	Object obj;
+	public boolean equals(Object obj) {
+		
+		System.out.println(obj instanceof Person);
+		if(obj instanceof Person) {
+			return id == ((Person)obj).id;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	//System.out.println(obj instanceof Person);
+	
+	Person(long id){
+		this.id = id;
+	}
+}
 
 public class Ex9_2 {
 
 	public static void main(String[] args) {
 		
-		Value v1 = new Value(10);
-		Value v2 = new Value(10);
+		Person p1 = new Person(8905051234567L);
+		Person p2 = new Person(8905051234567L);
 		
-		if(v1.equals(v2)) {
-			System.out.println("v1 is equals to v2");
+		if(p1.equals(p2)) {
+			System.out.println("p1 and p2 are same");
 		}
+		
 		else {
-			System.out.println("v1 is different with v2");
+			System.out.println("p1 and p2 are different");
 		}
+		
+
 	}
+
 }
 
-class Value{
-	
-	int value;
-	
-	Value(int value){
-		this.value = value;
-	}
-	
-	public boolean equals(Object obj) {
-		//return this == obj;  //it will compare addresses
-		
-		if(obj instanceof Value) {
-			
-			Value v = (Value)obj;
-			
-			return this.value == v.value;
-		}
-		else {
-			return false;
-		}
-		
-	}
-}
+
