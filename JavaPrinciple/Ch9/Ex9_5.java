09b15c00b55aed0378f4dc23ff612b3419a9a61f
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-30 09:46:06 -0400

    Create Ex9_5.java

---
+++ JavaPrinciple/Ch9/Ex9_5.java
@@ -0,0 +1,32 @@
+package ch09_Ex;
+
+class Card2{
+	String kind;
+	int number;
+	
+	Card2(){
+		this("SPADE", 1);
+	}
+	
+	Card2(String kind, int number){
+		this.kind = kind;
+		this.number = number;
+	}
+	
+	public String toString() {
+		return "kind : " + kind + " number : " + number;
+	}
+}
+
+public class Ex9_5 {
+
+	public static void main(String[] args) {
+		
+		Card2 c1 = new Card2("HEART", 10);
+		Card2 c2 = new Card2();
+		
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+	}
+
+}
