85e31693576b4eee0ef8796eea383354a03e35a1
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-07-31 23:11:38 -0400

    Create Ex9_7.java

---
+++ JavaPrinciple/Ch9/Ex9_7.java
@@ -0,0 +1,14 @@
+package ch09_Ex;
+
+public class Ex9_7 {
+
+	public static void main(String[] args) {
+		
+		String s1 = "AAA";
+		String s2 = "AAA";
+		String s3 = "AAA";
+		String s4 = "BBB";
+
+	}
+
+}
