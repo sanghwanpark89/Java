af369a082e6418afeba78e0323b90d69153d6ee7
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2023-10-01 23:43:04 -0400

    Update Main.java

--- leetCode/twoSum/Main.java
+++ leetCode/twoSum/Main.java
@@ -7,42 +7,42 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner myScan = new Scanner(System.in);
-		//Create the scanner instance
+		//Create the scanner instance.
 
 		System.out.println("Enter your array size");
-		//Ask to user that size of array user wants to make
+		//Ask to user that size of array user wants to make.
 		
 		int myArraySize = 0;
-		//Initialize the array size
+		//Initialize the array size.
 		
 		myArraySize = myScan.nextInt();
-		//User will provide the size of array, and scanner will save the numbers into myArraySize variable
+		//User will provide the size of array, and scanner will save the numbers into myArraySize variable.
 		
 		int[] myArray = new int[myArraySize];
-		//Declare the array with array size
+		//Declare the array with array size.
 		
 		System.out.println("Enter your array");
-		//Ask to user to provide array
+		//Ask to user to provide array.
 		
 		for(int i = 0; i < myArraySize; i++) {
 			myArray[i] = myScan.nextInt();
 		}
-		//for loop will save the array element and make the array
+		//for loop will save the array element and make the array.
 		
 		System.out.println("Enter your target number");
-		//Ask to user to provide target number
+		//Ask to user to provide target number.
 		
 		int myTarget = myScan.nextInt();
-		//Scanner will save the target number into myTarget variable
+		//Scanner will save the target number into myTarget variable.
 		
 		System.out.println(Arrays.toString(myArray));
-		//If you want to print array, you need to Arrays.toString() method to print out the array
+		//If you want to print array, you need to Arrays.toString() method to print out the array.
 		
 		TwoSum myTwoSum = new TwoSum();
-		//create the TwoSum object and the address will be saved into myTwoSum
+		//create the TwoSum object and the address will be saved into myTwoSum.
 		
 		System.out.println(Arrays.toString(myTwoSum.twoSum(myArray, myTarget)));
-		//This line will send the value of array and target number to Two Sum class then will print out the result
+		//This line will send the value of array and target number to Two Sum class then will print out the result.
 
 	}
 
