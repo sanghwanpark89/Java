797041ab35d1fd9df5557909e91c6683724bc6be
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2024-05-01 23:49:36 -0400

    Update Ex6_1.java

--- JavaPrinciple/Ch6/Ex6_1.java
+++ JavaPrinciple/Ch6/Ex6_1.java
@@ -4,11 +4,11 @@ public class Ex6_1 {
 
 	public static void main(String[] args) {
 		
-		Tv t = new Tv();
+		Tv t = new Tv()	// create tv instance
 		
-		t.channel = 7;
-		t.channelDown();
-		System.out.println("Current channel is " + t.channel);
+		t.channel = 7;	// assign number 7 tv.channel
+		t.channelDown();	//call channelDown() method
+		System.out.println("Current channel is " + t.channel);	//it will print number 7
 
 	}
 
@@ -18,9 +18,9 @@ public class Ex6_1 {
 class Tv{
 	//TV속성
 	
-	String color;	//색상 
+	String color;	//색상, color
 	boolean power; 	//전원상태(on/off)
-	int channel;	//채널 
+	int channel;	//채널, channel
 	
 	//TV의 기능(메서드 ) 
 	
