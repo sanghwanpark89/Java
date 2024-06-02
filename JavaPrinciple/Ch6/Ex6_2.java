053d54d85466eccd42bd6d3606d267aaeaf6afc1
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2024-06-01 23:08:50 -0400

    Update Ex6_2.java

--- JavaPrinciple/Ch6/Ex6_2.java
+++ JavaPrinciple/Ch6/Ex6_2.java
@@ -1,31 +1,50 @@
 package ch06_Ex;
 
-public class Ex6_2 {
+// public class Ex6_2 {
 
-	public static void main(String[] args) {
+// 	public static void main(String[] args) {
 		
-		Tv t = new Tv();
+// 		Tv t = new Tv();
 		
-		t.channel = 7;
-		t.channelDown();
-		t.channelDown();
-		t.channelUp();
+// 		t.channel = 7;
+// 		t.channelDown();
+// 		t.channelDown();
+// 		t.channelUp();
 		
-		System.out.println("Current channel is " + t.channel);
+// 		System.out.println("Current channel is " + t.channel);
 		
+// 		Tv t2 = new Tv();
+// 		t2.channel = 10;
+// 		t2.channelUp();
+// 		t2.channelUp();
+		
+// 		System.out.println("Current channel is "  + t2.channel);
+		
+		
+
+// 	}
+
+// }
+
+public class Ex6_2{
+
+	public static void main(String[] arts){
+
+		Tv t1 = new Tv();
 		Tv t2 = new Tv();
-		t2.channel = 10;
-		t2.channelUp();
-		t2.channelUp();
-		
-		System.out.println("Current channel is "  + t2.channel);
-		
-		
 
+		System.out.println("Value of T1 is " + t1.channel);
+		System.out.println("Value of T2 is " + t2.channel);
+
+		t1.channel = 7;
+		System.out.println("Value of T1 has been changed to 7");
+
+		System.out.println("Value of T1 is " + t1.channel);	//7
+		System.out.println("Value of T2 is " + t2.channel);	//0
+		
 	}
-
+	
 }
-
 //class Tv{
 //	
 //	String color;
