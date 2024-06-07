7c14dc7120b4a65ec28a4c5a8aae0e6dd28a5837
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2024-06-06 22:58:48 -0400

    Update Ex6_6.java

--- JavaPrinciple/Ch6/Ex6_6.java
+++ JavaPrinciple/Ch6/Ex6_6.java
@@ -11,11 +11,12 @@ public class Ex6_6 {
 		change(d.x);
 		System.out.println("After changed(d.x) " + d.x);
 		
-		System.out.println("maine() : x " + d.x);
+		System.out.println("maine() : x " + d.x);	//d.x의 값이 변경된 것이 아니라, change method의 매개변수 x의 값이 변경된 것이다. 
+								//즉, 원본이 아닌 복사본이 변경된 것이라 원본에는 아무런 영향을 미치지 못한것이다.
 
 	}
 	
-	static void change(int x) {
+	static void change(int x) {	//기본현 매개변수
 		x=1000;
 		System.out.println("change() : x = " + x);
 	}
