f262aab09d6ff8479d4b8b67dadfcabf28926138
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2025-01-01 20:05:51 -0500

    Update Ex7_1.java

--- JavaPrinciple/Ch7/Ex7_1.java
+++ JavaPrinciple/Ch7/Ex7_1.java
@@ -2,46 +2,59 @@ package ch7;
 
 class Tv{
 	
-	boolean power;
-	int channel;
+	boolean power;	//status of power (on/off)
+	
+	int channel;	//channel
 	
 	void power() {
 		power = !power;
 	}
+	
 	void channelUp() {
 		++channel;
 	}
+	
 	void channelDown() {
 		--channel;
 	}
-	
 }
 
-class SmartTv extends Tv{
+class SmartTv extends Tv{	// SmartTV has caption function. 
 	
-	boolean caption;
+	boolean caption;	//	Caption on/off
+	//	default value is false.
+	
 	void displayCaption(String text) {
-		if(caption) {	//Captino 상태가 on일때만 text를 출력한다.
+		if(caption) {	// if status of caption is true, it will show caption
 			System.out.println(text);
 		}
 	}
-	
 }
-
 
 public class Ex7_1 {
 
 	public static void main(String[] args) {
 		
 		SmartTv stv = new SmartTv();
-		stv.channel = 10;
-		stv.channelUp();
-		System.out.println(stv.channel);
 		
-		stv.displayCaption("Hello, World");
-		stv.caption = true;	//캡션 기능을 켠다
-		stv.displayCaption("Hello, World");
+		stv.channel = 10;      //	member From Parent
+		stv.channelUp();       //	member From parent
+		stv.channelDown();     //	member From parent
+		stv.channelDown();     //	member From parent
 
+		
+		System.out.println(stv.channel);
+		// 9
+		
+		stv.caption = true;
+		System.out.println(stv.caption);
+		//true
+		
+		stv.displayCaption("Hello World through method");
+		//hello world through method
+
+		stv.displayCaption("Hello World");
+		//hello world
 	}
 
 }
