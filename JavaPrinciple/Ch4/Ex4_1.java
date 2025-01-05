c4f9105449ff9cb73c899aae3e0e6b7c1d3db021
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2025-01-05 09:15:26 -0500

    Create Ex4_1.java

---
+++ JavaPrinciple/Ch4/Ex4_1.java
@@ -0,0 +1,18 @@
+package Ch4;
+
+public class Ex4_1 {
+
+	public static void main(String[] args) {
+		
+		int score = 80;
+		
+		if(score > 60) {
+			System.out.println("Pass");
+		}
+	}
+}
+
+/**
+ * if the condition from parenthesis is true, the statement in {} will be executed.
+ * if the condition from parenthesis is false, the statement in {} will not be executed.
+ */
