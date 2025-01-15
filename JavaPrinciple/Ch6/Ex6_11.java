cc41443186cfbb8e4aaa6dfb75dc078988cae41b
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2025-01-14 23:16:31 -0500

    Update Ex6_11.java

--- JavaPrinciple/Ch6/Ex6_11.java
+++ JavaPrinciple/Ch6/Ex6_11.java
@@ -1,27 +1,27 @@
-package ch6;
-
-class Data_1{
-	int value;
-}
-
-class Data_2{
-	
-	//Data_2(){}
-	
-	int value;
-	Data_2(int x){	//매개변수가 있는 생성자
-		value = x;
-	}
-}
+package ch06_Ex;
 
 public class Ex6_11 {
 
 	public static void main(String[] args) {
 		
 		Data_1 d1 = new Data_1();
+		Data_2 d2 = new Data_2();	//compile error
 		
-		Data_2 d2 = new Data_2();	//Compile error 발생
 
 	}
 
+}
+
+class Data_1{
+	int value;
+}
+
+class Data_2{
+	int value;
+	Data_2(){
+		
+	}
+	Data_2(int x){
+		value = x;
+	}
 }
